0ffb3ccba5fac82db2d26dc7823711643fab6795
Dickson Prince <dicksoniprince@gmail.com>
2020-02-01 17:57:03 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+        $(document).ready(function () {
+            // alert('I see you')
+            //* About page Project sort  
+            $('.webApp').hide();
+            $('.app').click(function () {
+                // alert('try again');
+                $('.webApp').show();
+            })
+        });
